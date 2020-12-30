04ff7734d1d2c87e328b1a733a0a0b5d23c3d85a
lluis31 <76479676+lluis31@users.noreply.github.com>
2020-12-30 10:02:21 +0100

    restaurant

--- exercici-restaurant/Program.cs
+++ exercici-restaurant/Program.cs
@@ -1,58 +1,79 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace exercici_restaurant
+namespace ConsoleApp1
 {
     class Program
     {
-        public int Plats { get; set; }
-        public int Preu { get; set; }
-        
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //fase1
-            int bill5= 5, bill10= 10, bill20= 20, bill50= 50, bill100= 100, bill200 = 200, bill500 = 500;
-            int preutotal;
+            int b5 = 5, b10 = 10, b20 = 20, b50 = 50, b100 = 100, b200 = 200, b500 = 500;
+            double total = 0;
+            string[] menu = { "pizza", "hamburguesa", "entrecot", "spaguetti", "dorada", "sepia" };
+            int[] precio = { 8, 10, 15, 8, 18, 20 };
 
-            Console.WriteLine("Quants plats vol que tingui en menu");
-            int numplats = Convert.ToInt32(Console.ReadLine());
-
-            string[] menu = new string[numplats];
-            int[] preus = new int[numplats];
-
-            //fase 2
-            for (int i = 0; i < numplats; i++)
-
+            Dictionary<string, int> plato = new Dictionary<string, int>();
+            for (int i = 0; i < menu.Length; i++)
             {
-
-                Console.WriteLine("Introdueix el nom del plat {0}:", i + 1);
-
-                menu[i] = Console.ReadLine();
-
-                Console.WriteLine("Introdueix el preu del plat {0} ({1}):", i + 1, menu[i]);
-
-                preus[i] = Convert.ToInt32(Console.ReadLine());
-
-                Console.Clear();
-
-                Console.WriteLine("---EXERCICI RESTAURANT---");
-
-                Console.WriteLine("");
-
+                plato.Add(menu[i], precio[i]);
             }
 
-            Dictionary<string, int> openWith = new Dictionary<string, int>();
-
-            for (int i = 0; i < numplats; i++)
-
+            Dictionary<string, int> dict = new Dictionary<string, int>(plato);
+            foreach (KeyValuePair<string, int> kvp in dict)
             {
-
-                openWith.Add(menu[i], preus[i]);
-
+                Console.WriteLine("Plato : {0}, Precio: {1} euros",
+                   kvp.Key, kvp.Value);
             }
 
-            Console.Clear();
+            Console.WriteLine("Que desea para comer?");
 
+
+            ArrayList pedidoAL = new ArrayList();
+            bool v = false;
+            do
+            {
+                string introducirPedido = Console.ReadLine().ToLower();
+
+                if (menu.Contains(introducirPedido))
+                {
+                    pedidoAL.Add(introducirPedido);
+
+                    if (dict.ContainsKey(introducirPedido))
+                    {
+                        foreach (KeyValuePair<string, int> kvp in dict)
+                        {
+                            total = total + kvp.Value;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} producto inexistente", introducirPedido);
+                }
+
+                Console.WriteLine("Desea algo mas? \n[Seleccione 1 si su respuesta es SI] [Seleccione 0 si su respuesta es NO]");
+                int algoMas = Convert.ToInt32(Console.ReadLine());
+
+                if (algoMas == 1)
+                {
+                    Console.WriteLine("Va a pedir algo mas?");
+                }
+                else
+                {
+                    Console.WriteLine("Cuenta");
+                    foreach (string x in pedidoAL)
+                    {
+                        Console.WriteLine(x);
+
+                    }
+                    Console.WriteLine("El total de su comida es {0}", total);
+                    v = true;
+                }
+
+            } while (!v);
         }
+
     }
 }
